3124a607e13298140723dad3dd6e093a5bb12f29
murtazahassan <mhpr@live.co.uk>
2019-11-11 09:09:50 +0300

    Update Read_Image_Video_Webcam.py

--- Basics/Read_Image_Video_Webcam.py
+++ Basics/Read_Image_Video_Webcam.py
@@ -1,24 +1,23 @@
 
 ######################## READ IMAGE ############################
-import cv2
-# LOAD AN IMAGE USING 'IMREAD'
-img = cv2.imread("Resources/lena.png")
-# DISPLAY
-cv2.imshow("Lena Soderberg",img)
-cv2.waitKey(0)
+# import cv2
+# # LOAD AN IMAGE USING 'IMREAD'
+# img = cv2.imread("Resources/lena.png")
+# # DISPLAY
+# cv2.imshow("Lena Soderberg",img)
+# cv2.waitKey(0)
 
 ######################### READ VIDEO #############################
-import cv2
-frameWidth = 640
-frameHeight = 480
-cap = cv2.VideoCapture("Resources/testVideo.mp4")
-while True:
-    success, img = cap.read()
-    img = cv2.resize(img, (frameWidth, frameHeight))
-    cv2.imshow("Result", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
+# import cv2
+# frameWidth = 640
+# frameHeight = 480
+# cap = cv2.VideoCapture("Resources/testVideo.mp4")
+# while True:
+#     success, img = cap.read()
+#     img = cv2.resize(img, (frameWidth, frameHeight))
+#     cv2.imshow("Result", img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
 ######################### READ WEBCAM  ############################
 import cv2
 frameWidth = 640
