e861d7cdfacd8c0188a1739181f7cab2b58cbf48
murtazahassan <mhpr@live.co.uk>
2020-02-07 19:27:40 +0300

    Update Joining_Multiple_Images_To_Display.py

--- Basics/Joining_Multiple_Images_To_Display.py
+++ Basics/Joining_Multiple_Images_To_Display.py
@@ -9,7 +9,7 @@ cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 
-def stackImages(scale,imgArray):
+def stackImages(imgArray,scale,lables=[]):
     rows = len(imgArray)
     cols = len(imgArray[0])
     rowsAvailable = isinstance(imgArray[0], list)
@@ -18,26 +18,31 @@ def stackImages(scale,imgArray):
     if rowsAvailable:
         for x in range ( 0, rows):
             for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
                 if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
         imageBlank = np.zeros((height, width, 3), np.uint8)
         hor = [imageBlank]*rows
         hor_con = [imageBlank]*rows
         for x in range(0, rows):
             hor[x] = np.hstack(imgArray[x])
+            hor_con[x] = np.concatenate(imgArray[x])
         ver = np.vstack(hor)
+        ver_con = np.concatenate(hor)
     else:
         for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
             if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
         hor= np.hstack(imgArray)
+        hor_con= np.concatenate(imgArray)
         ver = hor
+    if len(lables) != 0:
+        eachImgWidth= int(ver.shape[1] / cols)
+        eachImgHeight = int(ver.shape[0] / rows)
+        print(eachImgHeight)
+        for d in range(0, rows):
+            for c in range (0,cols):
+                cv2.rectangle(ver,(c*eachImgWidth,eachImgHeight*d),(c*eachImgWidth+len(lables[d][c])*13+27,30+eachImgHeight*d),(255,255,255),cv2.FILLED)
+                cv2.putText(ver,lables[d][c],(eachImgWidth*c+10,eachImgHeight*d+20),cv2.FONT_HERSHEY_COMPLEX,0.7,(255,0,255),2)
     return ver
     
 while True:
